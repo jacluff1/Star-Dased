99f30c7ea070b917503b4b8659d99b8fa168f4f1
domdobis <dominicdobis@gmail.com>
2019-10-20 14:31:40 -0700

    Prototyped the Linear Regression Model but needs to be properly connected to the sim

--- pyFiles/MetaModels/LinearRegression.py
+++ pyFiles/MetaModels/LinearRegression.py
@@ -4,6 +4,8 @@
 #===============================================================================#
 
 from pyFiles.BaseClass import BaseClass
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 from MLbase import MLbase
 
 #===============================================================================#
@@ -22,6 +24,16 @@ class PolynomialRegression( BaseClass, MLbase ):
 
     def __init__( self, *args, **kwargs ):
         pass
+
+    #Note need to figure the index scheme or the resulting output sample structure that would be fed to the metaModels
+    def run(self, *args, **kwargs ):
+        poly_Features = PolynomialFeatures(degree=8) #Based on the number of features 
+        x_poly = poly_Features.fit_transform(self.sample_) #Relating to the control factor columns
+        
+        poly_Features.fit(x_poly,self.sample_ )#Relating to the survival orbit column that is deduced from whether escape or collided
+        lin = LinearRegression()
+        lin.fit(X_poly,y);
+        
 
     #===========================================================================#
     # puplic methods                                                            #
