505b86400cbc34802292c3662631c9583634fdbe
domdobis <dominicdobis@gmail.com>
2019-10-20 23:02:53 -0700

    Moved the Exploratory Analysis into Plots.py script and removed EDA folder

--- pyFiles/Plots.py
+++ pyFiles/Plots.py
@@ -14,6 +14,24 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 import numpy as np
+from pyFiles.BaseClass import BaseClass
+from pyFiles.Simulation import Simulation
+
+#===========================================================================
+#Exploratory Analysis plot functions
+#===========================================================================
+class ExploratoryDataAnalysis(BaseClass,Simulation):
+
+    def __init__(self, *args, **kwargs):
+        pass
+     
+    def hist_plot(self, *args, **kwargs):
+        ax = self.sample_.hist(columns='survive') #Using the dataFrame that has the results from Sim for all the treatments
+        ax.set_xlabel("Number of Occurences")
+        ax.set_ylabel("Probability of Three Star Orbit Survive")
+
+    def box_plot(self, *args, **kwargs):
+        boxplot = self.sample_.boxplot(column=['survive']
 
 #===============================================================================#
 # static 3D plot of positions                                                   #
@@ -116,3 +134,7 @@ if __name__ == "__main__":
 
     staticPositionPlot( **kwargs )
     animation( **kwargs )
+
+    explore = ExploratoryDataAnalysis()
+    explore.hist_plot()
+    explore.box_plot()
