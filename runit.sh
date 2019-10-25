c0583de2c21cdbc40004e62c50e39ecf284bdf10
jacluff1 <jacluff1@asu.edu>
2019-10-24 22:00:34 -0700

    ran sim with limits between 20 & 200 AU. Expanded use of runit.sh and __main__ in pyFiles/Plots.py to allow arguments being passed through command line

--- runit.sh
+++ runit.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # script tested on Ubuntu 18.04.3 LTS using Python 3.6.8
 
+# activate StarDasedEnv
+source activateEnv.sh
+
 python -B pyFiles/Simulation.py
+python -B pyFiles/Plots.py --positionPlot True --animation True
 # python -B pyFiles/MetaModels/PolynomialRegression
 # python -B pyFiles/MetaModels/RegressionBasisFunctions
 # python -B pyFiles/MetaModels/RandomForests
